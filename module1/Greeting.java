71d98dd96f392ffd76c5de0e9be4475025961fe9
Nana Thompson <abee.ku23@gmail.com>
2026-09-03 23:44:51 -0400

    Add Module 1 activity: Hello World and Greeting programs

---
+++ module1/Greeting.java
@@ -0,0 +1,14 @@
+package bootcamp.Module1;
+import java.util.Scanner; 
+ 
+public class Greeting { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("What is your name? "); 
+        String name = scanner.nextLine(); 
+        System.out.print("What do you do for work? "); 
+        String work = scanner.nextLine();
+        System.out.println("Hello, " + name + "! You work as a " + work + ". Welcome to the program."); 
+        scanner.close(); 
+    } 
+}
